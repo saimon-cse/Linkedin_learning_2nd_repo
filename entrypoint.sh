83f2c166a672284a5e5f7d1a52f6591eb5e00736
Saimon Islam <147239993+saimon-cse@users.noreply.github.com>
2024-11-03 06:02:54 +0000

    update entrypoin.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,13 @@
 
 echo "=============="
 
-git config --global user.name $"{GITHUB_ACTOR}"
-git config --global user.email $"{INPUT_EMAIL}"
-git config --global -add safe.directory /github/workspace
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
 
-python3 /usr/bin/feed.py
+python3 /usr/bin/feed.py  # Fixed path to /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
-
 git push --set-upstream origin main 
 
 echo "=============="
